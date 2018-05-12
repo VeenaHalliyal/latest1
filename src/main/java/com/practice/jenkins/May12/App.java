2fcf747266d08783814acec21f04a5575b385c7b
veena <vhalliyal@gmail.com>
2018-05-13 00:00:30 +0530

    a

---
+++ src/main/java/com/practice/jenkins/May12/App.java
@@ -0,0 +1,13 @@
+package com.practice.jenkins.May12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/practice/jenkins/May12/Test1.java
@@ -0,0 +1,42 @@
+package com.practice.jenkins.May12;
+
+import org.testng.annotations.*;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+public class Test1 {
+	
+	public WebDriver driver;
+	@BeforeMethod
+	public void openAppln()
+	{
+	String key="webdriver.gecko.driver";
+	String value="./Softwares/geckodriver.exe";
+	System.setProperty(key, value);
+	 driver=new FirefoxDriver();
+	driver.get("https://en-gb.facebook.com/login/");
+	
+	}
+	@Test
+	public void testLogin() throws InterruptedException
+	{
+	driver.findElement(By.id("email")).sendKeys("testlogin567@gmail.com");
+	driver.findElement(By.id("pass")).sendKeys("qspiders123");
+	driver.findElement(By.id("loginbutton")).click();
+	Thread.sleep(2000);
+	String title=driver.getTitle();
+	System.out.println(title);
+	AssertJUnit.assertEquals(title, "Facebook");
+	System.out.println("statement after assert");
+	}
+
+@AfterMethod
+public void closeAppln() throws InterruptedException
+{
+Thread.sleep(4000);
+	driver.quit();
+}
+}
